b068d10296e25112b9dfde7f2ba4222d4c4a0fb1
Ayobami-S-O <damilolaode01@gmail.com>
2022-05-30 09:22:33 +0100

    ch:worked on Js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,20 @@
-// document.write('<h1>Hello World</h1>');
 
-// document.querySelector('.first').innerHTML = 'Welcome Home';
-
-// // alert('Hello Somebody');
-
-// console.log('Hello Ayo');
-
-// function greetMe() {
-//  alert("Good Morning Everyone")
-// }
-
-// let noun = 'Samson';
-// let verb = 'kill';
-// let adjective = 'big'
-
-// console.log(`${noun} ${verb} ${adjective}`)
-
+// (1)
 document.querySelector('.groupName').innerHTML = 'Group Name:';
 
-document.querySelector('.name').innerHTML = 'Team Atemis';
+document.querySelector('.name').innerHTML = 'Team Artemis';
 
 document.querySelector('.groupName').style.color = "lightgreen";
 
 document.querySelector('.name').style.color = "purple";
 
-let teamName = ['Merit', 'Abdulsamad', 'Comely', 'Gabriel-Eta Ekpo', 'Emmanuel Stephen', 'Ayobami S O', 'Danny'];
-console.log(teamName[5]);
+// (2)
 
+let teamName = ['Merit', 'Abdulsamad', 'Comely', 'Gabriel-Eta Ekpo', 'Emmanuel Stephen', 'Ayobami S O', 'Danny'];
+console.log(`My name is ${teamName[5]}`);
+
+
+// (3)
 let profile = {
     firstName: 'Ayobami',
     lastName: 'Ogundiya',
@@ -38,30 +26,32 @@ let profile = {
     groupName:    'Team Artemis'
 };
 
-console.log(profile.bestMovie)
+console.log(`My best movie is ${profile.bestMovie}`)
 
+  
+// (4)
 let noun = 'Samuel';
 let verb = 'plays';
 let adjective = 'nice'
 
-console.log(`${noun} is ${adjective}`);
-console.log(`${noun} ${verb} ${adjective} football`);
-console.log(`${noun} loves to ${verb}`);
-console.log(`He ${verb} ${adjective} music`);
-console.log(`${noun} ${verb} hockey`);
+console.log(`First sentence: ${noun} is ${adjective}`);
+console.log(`Second sentence: ${noun} ${verb} ${adjective} football`);
+console.log(`Third sentence: ${noun} loves to ${verb}`);
+console.log(`Fourth sentence: He ${verb} ${adjective} music`);
+console.log(`Fift sentence: ${noun} ${verb} hockey`);
 
 
 
-(5)
+// (5)
 let x = myFunction(25,4);
 
 function myFunction(a,b) {
     return (a % b);
 }
-console.log(x);
+console.log(`The remainder of 25 divide by 4 is: ${x}`);
 
 
-(6)
+// (6)
 let d = quadraticFormula(4,4,6);
 let y = quadraticFormula(4,4, 6);
 
@@ -70,28 +60,31 @@ function quadraticFormula(a, b,c) {
     const y = (((-1*b) - Math.sqrt(Math.pow(b,2)) - (4*a*c))/(2*a));
     return `${d} , ${y}`
 };
-console.log(d, y);
+console.log(`The root of the quadratic equation is X = ${d}`);
 
 
-(7)
+// (7)
 
 let myNoun = "dog";
 const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
+const wordBlanks = `My ${myAdjective} ${myNoun} ${myVerb} ${myAdverb} towards me`;
 
-console.log(`My ${myAdjective} ${myNoun} ${myVerb} ${myAdverb} towards me`);
+console.log(wordBlanks);
 
 
 // (8)
 
-let a = areaOfCircle(3.142, 9);
 
-function areaOfCircle(pi, r) {
-  
-   return pi * (Math.pow(r,2));
+const pi = 3.142;
+let area = calculateArea(9);
+function calculateArea(radius) {
+    var area = pi * (Math.pow(radius,2));
+    return area;
 }
-console.log(a);
+
+console.log(`The area of the circle of radius 9cm is ${area}`);
 
 // (9)
 
@@ -99,7 +92,7 @@ let p = 8200;
 let r = 17.5;
 let t = 2.5;
 const SI = (p*r*t)/100;
-console.log(SI);
+console.log(`The Simple Interest is ${SI}`);
 
 // (10)
 
@@ -107,7 +100,7 @@ const c = 10;
 const e = 4
 let rem = c % e;
 
-console.log(rem);
+console.log(`The remainder is ${rem}`);
 
 
 // (11)
@@ -121,7 +114,10 @@ const nh = 1.95;
 let BMIM = mw/(mh*mh);
 let BMIN = nw/(nh*nh);
 
-console.log(BMIM, BMIN);
+console.log( `Merit Higher BMI data 1 is: ${BMIM} while Nutjob BMI data 1 is ${BMIN}`);
+
+let meritHigherBMI1 = BMIM > BMIN;
+console.log(`Merit Higher BMI for data 1 is ${meritHigherBMI1}`)
    
 
 // Test 2
@@ -135,7 +131,8 @@ const mh2 = 1.76;
 let BMIN2 = nw/(nh*nh);
 let BMIM2 = mw/(mh*mh);
 
-console.log(BMIN2, BMIM2)
+console.log(`Nutjob BMI data 2 is: ${BMIN2} while Merit BMI data 2 is ${BMIM2}`)
 
-const meritHigherBMI = BMIM > BMIN;
+let meritHigherBMI2 = BMIM2 > BMIN2;
+console.log(`Merit Higher BMI for data 2 is ${meritHigherBMI2}`)
 
